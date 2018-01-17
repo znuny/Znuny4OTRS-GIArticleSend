f3f72e6e10bc6d6d00fbcb03ffe1687f9220a51c
Jens Pfeifer <jens.pfeifer@znuny.com>
2018-01-17 08:16:48 +0000

    Applied code policy.

--- Custom/Kernel/GenericInterface/Operation/Ticket/Common.pm
+++ Custom/Kernel/GenericInterface/Operation/Ticket/Common.pm
@@ -1,6 +1,6 @@
 # --
-# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
-# Copyright (C) 2012-2017 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2001-2018 OTRS AG, http://otrs.com/
+# Copyright (C) 2012-2018 Znuny GmbH, http://znuny.com/
 # --
 # $origin: otrs - 2be0a4540ffd992654d13728e82a63d9040e1a3a - Kernel/GenericInterface/Operation/Ticket/Common.pm
 # --
