faa99ac7c7259a3f6659947373273b567606c3cc
Denny Korsukéwitz <dk@znuny.com>
2021-01-20 13:15:12 +0100

    Happy new year. Tidied code.

--- Custom/Kernel/GenericInterface/Operation/Ticket/TicketUpdate.pm
+++ Custom/Kernel/GenericInterface/Operation/Ticket/TicketUpdate.pm
@@ -1,6 +1,6 @@
 # --
-# Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2012-2020 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
+# Copyright (C) 2012-2021 Znuny GmbH, http://znuny.com/
 # --
 # $origin: otrs - daad50a0cfe54012a40624f378c0ab509c30b7d6 - Kernel/GenericInterface/Operation/Ticket/TicketUpdate.pm
 # --
